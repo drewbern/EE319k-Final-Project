dc149892f142d65b8632ec0244587454d07d361f
Philip Tan <philtan123@gmail.com>
2017-04-28 22:32:41 -0500

    Appropriate header comments to Main

--- StarFoxmain.c
+++ StarFoxmain.c
@@ -1,27 +1,6 @@
-// Lab7Main.c
-// Runs on LM4F120/TM4C123
-// Test the functions in ST7735.c by printing basic
-// patterns to the LCD.
-//    16-bit color, 128 wide by 160 high LCD
-// Daniel Valvano
-// Last Modified: 3/6/2015
-// Ramesh Yerraballi modified 3/20/2017
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2014
-
- Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
+// StarFox Game for Lab 10 Competition
+// By Philip Tan and Zach Chilton
+// Spicy game
 
 // Backlight (pin 10) connected to +3.3 V
 // MISO (pin 9) unconnected 
